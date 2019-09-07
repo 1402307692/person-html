02a9dff7c40a902ab93f77953147a0f2ebb368d9
1402307692 <39395531+1402307692@users.noreply.github.com>
2019-09-07 21:25:46 +0800

    Add files via upload

---
+++ js/cander.js
@@ -0,0 +1,52 @@
+#HCQ_calendar{
+  color:white;
+  width: 400px;
+  height: 400px;
+  background: black;
+  opacity: .8;
+  border:1px solid #888;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  -khtml-user-select: none;
+  user-select: none;
+}
+#HCQ_date_time{
+  width: 97%;
+  margin-left: 3%;
+}
+#HCQ_date{
+  text-align: center;
+  float: left;
+  width: 40%;
+}
+#HCQ_time{
+  text-align: center;
+  float: left;
+  width: 30%;
+}
+#HCQ_oprate input{
+  width:10%;
+  background-color:rgb(100, 109, 100);
+  border:none;
+  color:black;
+  display:inline-block;
+  cursor: pointer;    
+}
+#HCQ_table{
+  color: white;
+  width:90%;
+  height:90%;
+  margin-left: 5%;
+  text-align: center;
+}
+#HCQ_table td{
+  width: 14%;
+  height: 14%;
+}
+#HCQ_table .td0{
+  color: rgb(73, 73, 73);
+}
+#HCQ_table .td2{
+  color: rgb(73, 73, 73);
+}
